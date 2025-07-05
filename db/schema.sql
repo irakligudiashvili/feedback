777e161ab22e1cd786eb7adc14f47732c51637d8
Irakli Gudiashvili <igudiashvili99@gmail.com>
2025-07-06 01:03:48 +0400

    create schema

---
+++ db/schema.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    email TEXT UNIQUE NOT NULL,
+    password_hash TEXT NOT NULL,
+    role TEXT CHECK(role IN('customer', 'manager', 'cook', 'driver')) DEFAULT 'customer',
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
