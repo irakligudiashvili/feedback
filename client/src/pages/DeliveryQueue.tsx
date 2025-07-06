12e537ee88555ba78096d8b6a4fa7c3264157369
Irakli Gudiashvili <igudiashvili99@gmail.com>
2025-07-06 20:01:16 +0400

    created pages

---
+++ client/src/pages/DeliveryQueue.tsx
@@ -0,0 +1,7 @@
+function DeliveryQueue(){
+    return (
+        <h1>Delivery Queue</h1>
+    )
+}
+
+export default DeliveryQueue
---
+++ client/src/pages/KitchenQueue.tsx
@@ -0,0 +1,7 @@
+function KitchenQueue(){
+    return (
+        <h1>Kitchen Queue</h1>
+    )
+}
+
+export default KitchenQueue
