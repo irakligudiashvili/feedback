df88f5b862ff862d7c49725d14ae51df44c406f1
Irakli Gudiashvili <igudiashvili99@gmail.com>
2025-07-09 02:26:07 +0400

    added menu_items schema

---
+++ db/menu_items_schema.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS menu_items(
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    description TEXT,
+    ingredients TEXT[],
+    price NUMERIC(10, 2) NOT NULL,
+    available BOOLEAN NOT NULL,
+    image_url TEXT
+)
